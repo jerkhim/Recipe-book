2442fc13313d5fbd6fef27eae3ae60bf373f68e5
Your Name <riveraleeryan130@gmail.com>
2024-05-24 17:47:20 +0800

    try

--- src/app/_models/recipe.ts
+++ src/app/_models/recipe.ts
@@ -4,10 +4,6 @@ export class Recipe {
   ingredients: string[];
   instructions: string[];
 
-  constructor(name: string, description: string, ingredients: string[], instructions: string[]) {
-    this.name = name;
-    this.description = description;
-    this.ingredients = ingredients;
-    this.instructions = instructions;
+
   }
-}
+
